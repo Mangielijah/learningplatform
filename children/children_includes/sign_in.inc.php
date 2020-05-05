872d041440eed48a9a9b4cb5b4a270d9de95e400
Ngang muma <ngangmuma00@gmail.com>
2020-05-05 15:02:53 -0700

    error page

--- children/children_includes/sign_in.inc.php
+++ children/children_includes/sign_in.inc.php
@@ -1,28 +1,30 @@
 <?php
-echo "string";
+
  require "../../includes/db.inc.php";
 if (isset($_POST['signin'])) {
 
-echo "<br>";
-  echo  $phoneNumber = $_POST['phoneNumber'];
- echo   $pwd = $_POST['pwd'];
- echo "<br>";
+
+    $phoneNumber = $_POST['phoneNumber'];
+   $pwd = $_POST['pwd'];
+
+echo    $hashedpassword = password_hash($pwd, PASSWORD_DEFAULT);
+
     
-        $sql = "SELECT * FROM children WHERE phoneNumber = ? and password = ?";
+        $sql = "SELECT * FROM children WHERE phoneNumber = ? and  password = ?";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt,$sql)) {
             header("Location:../index.php?error=sql error");
             exit();
         }
         else{
-            mysqli_stmt_bind_param($stmt,"ss",$phoneNumber,$pwd);
+            mysqli_stmt_bind_param($stmt,"ss",$phoneNumber,$hashedpassword);
             mysqli_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
            
             
             while ($row = mysqli_fetch_assoc($result)) {
                 $passwordCheck = password_verify($pwd,$row['password']);
-                
+
                 if ($passwordCheck == false) {
                     header("Location:../index.php?error=Incorrect Password");
                     exit();
@@ -44,8 +46,8 @@ echo "<br>";
             }
         }
     
-    // mysqli_stmt_close($stmt);
-    // mysqli_close($conn);
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 }
 else{
      header("Location:../index.php");
