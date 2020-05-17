1fab7473addede151dc33663bcf136870ba711f0
Ngang muma <ngangmuma00@gmail.com>
2020-05-17 15:08:33 -0700

    ui link added

--- children/childrenUI.php
+++ children/childrenUI.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Childrens Oage</title>
-	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1-dist/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="index2.css">
+  <?php include '../includes/styles_js_importer.php'; ?>
+<!-- 	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1-dist/css/bootstrap.min.css"> -->
+<!-- 	<link rel="stylesheet" type="text/css" href="index2.css"> -->
   <style type="text/css">
     
       h1{
