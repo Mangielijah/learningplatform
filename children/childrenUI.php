e7b2c2a689ba0f9baa888fb73f10bf68a8ed7194
Ngang muma <ngangmuma00@gmail.com>
2020-05-17 10:01:56 -0700

    children dashboard

---
+++ children/childrenUI.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Childrens Oage</title>
+	<link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="index2.css">
+  <style type="text/css">
+    
+      h1{
+        color: white;
+      }
+
+      .col-md-4{
+        margin-bottom: 20px;
+      }
+
+  </style>
+</head>
+<body class="bg-primary">
+<section>
+	<div class="container">
+		<!-- nav barr............................................... -->
+           <div class="row p-4">
+                <div class="col-md-4">
+                    <div class="pray text-center  shadow-sm p-3 mb-5 bg-white rounded">
+                      <img src="images/new/cross.png" alt="image" height="120px"><br><br>
+                      <a href="#" class="btn btn-primary py-2 px-8">INCOMPLETED LESSON</a>
+                    </div>
+                </div>
+                   <div class="col-md-4">
+                    <div class="pray text-center  shadow-sm p-3 mb-5 bg-white rounded">
+                      <img src="images/new/tick.webp" alt="image" height="120px"><br><br>
+                      <a href="#" class="btn btn-primary py-2 px-8">COMPLETED LESSON</a>
+                    </div>
+                </div>
+                  <div class="col-md-4">
+                    <div class="pray text-center  shadow-sm p-3 mb-5 bg-white rounded">
+                      <img src="images/new/qa.png" alt="image" height="120px"><br><br>
+                      <a href="#" class="btn btn-primary py-2 px-8">LESSON QUESTIONS</a>
+                    </div>
+                </div>
+           </div>
+
+	<h1 class=" text-center">INCOMPLETED LESSONS</h1><br>
+
+	      <!-- incompleted lessons ................................................................-->
+                    <div class="row p-4">
+                    <div class="col-md-4 text-center">
+                              <div class="card">
+
+                    <div class="card-img">
+                         <img src="images/new/environment.jpg" alt="image" class="img-responsive p-4 " height="200">
+                    </div>
+                    <div class="card-body">
+                         <h3 class="card-title">Learn Environmental Skills</h3>
+                         <hr>
+                         <a href="#" class="btn btn-primary py-2 px-8">VIEW LESSON</a>
+                    </div>
+               </div>
+
+                    </div>
+                         <div class="col-md-4 text-center">
+                              <div class="card">
+
+                    <div class="card-img">
+                         <img src="images/new/thermometer.jpg" alt="image" class="img-responsive p-4 " height="200">
+                    </div>
+                    <div class="card-body">
+                         <h3 class="card-title">Health And-<br> Sanitation </h3>
+                         <hr>
+                         <a href="#" class="btn btn-primary py-2 px-8">VIEW LESSON</a>
+                    </div>
+               </div>
+
+                    </div>
+                         <div class="col-md-4 text-center">
+                              <div class="card">
+
+                    <div class="card-img">
+                         <img src="images/new/hd.jpg" alt="image" class="img-responsive p-4 " height="200">
+                    </div>
+                    <div class="card-body">
+                         <h3 class="card-title">Learn 3D Shapes-<br>And Figures</h3>
+                         <hr>
+                         <a href="#" class="btn btn-primary py-2 px-8">VIEW LESSON</a>
+                    </div>
+               </div>
+
+                    </div>
+               </div><br><br>
+
+		<!-- incompleted lessons end here  -->
+         
+          	
+          		
+          				<h1 class=" text-center">COMPLETED LESSONS</h1><br>
+          		
+          
+          
+<!-- completed lessons....................................................... -->
+
+          	<div class="row p-4">
+          		<div class="col-md-4 text-center">
+          			 	<div class="card">
+
+          		<div class="card-img">
+          			<img src="images/8.webp" alt="image" class="img-responsive p-4 " height="200">
+          		</div>
+          		<div class="card-body">
+          			<h3 class="card-title">Sheep Farm Lengh & Mass</h3>
+          			<hr>
+          			<a href="#" class="btn btn-primary py-2 px-8">REVIEW LESSON</a>
+          		</div>
+          	</div>
+
+          		</div>
+          			<div class="col-md-4 text-center">
+          			 	<div class="card">
+
+          		<div class="card-img">
+          			<img src="images/new/sheep.jpg" alt="image" class="img-responsive p-4 " height="200">
+          		</div>
+          		<div class="card-body">
+          			<h3 class="card-title">Sheep Farm Lengh & Mass</h3>
+          			<hr>
+          			<a href="#" class="btn btn-primary py-2 px-8">REVIEW LESSON</a>
+          		</div>
+          	</div>
+
+          		</div>
+          			<div class="col-md-4 text-center">
+          			 	<div class="card">
+
+          		<div class="card-img">
+          			<img src="images/new/hd.jpg" alt="image" class="img-responsive p-4 " height="200">
+          		</div>
+          		<div class="card-body">
+          			<h3 class="card-title">Sheep Farm Lengh & Mass</h3>
+          			<hr>
+          			<a href="#" class="btn btn-primary py-2 px-8">REVIEW LESSON</a>
+          		</div>
+          	</div>
+
+          		</div>
+          		<div class="col-md-4 text-center">
+          			 	<div class="card">
+
+          		<div class="card-img">
+          			<img src="images/new/maths.jpg" alt="image" class="img-responsive p-4 " height="200">
+          		</div>
+          		<div class="card-body">
+          			<h3 class="card-title">Learning To count</h3>
+          			<hr>
+          			<a href="#" class="btn btn-primary py-2 px-8">REVIEW LESSON</a>
+          		</div>
+          	</div>
+
+          		</div>
+          			<div class="col-md-4 text-center">
+          			 	<div class="card">
+
+          		<div class="card-img">
+          			<img src="images/new/Language.jpg" alt="image" class="img-responsive p-4 " height="200">
+          		</div>
+          		<div class="card-body">
+          			<h3 class="card-title">Language Skills</h3>
+          			<hr>
+          			<a href="#" class="btn btn-primary py-2 px-8">REVIEW LESSON</a>
+          		</div>
+          	</div>
+
+          		</div>
+          			<div class="col-md-4 text-center">
+          			 	<div class="card">
+
+          		<div class="card-img">
+          			<img src="images/new/music.jpg" alt="image" class="img-responsive p-4 " height="200">
+          		</div>
+          		<div class="card-body">
+          			<h3 class="card-title">Music Skills</h3>
+          			<hr>
+          			<a href="#" class="btn btn-primary py-2 px-8">REVIEW LESSON</a>
+          		</div>
+          	</div>
+
+          		</div>
+          	</div><br><br>
+		
+    </div>
+</section>
+</body>
+</html>
